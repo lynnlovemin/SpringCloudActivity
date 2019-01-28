83696af107eb4a52ef547e6c296f6a8120f74748
lynnlovemin <331509758@qq.com>
2019-01-28 17:54:46 +0800

    更新

--- demo-lesson-one/src/main/java/com/lynn/boot/Application.java
+++ demo-lesson-one/src/main/java/com/lynn/boot/Application.java
@@ -6,15 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application  extends SpringBootServletInitializer {
+public class Application /* extends SpringBootServletInitializer */{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
 }
 
